d3a13d0a97f2aa5988cb02cc800ead1d3a04f678
vakyla98 <vakyla98@ukr.net>
2020-03-31 15:15:31 +0300

    /

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
 
 gulp.task('sass', async function () {
   gulp.src('./src/css/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());;
 });
 
 gulp.task('sass:watch', async function () {
   gulp.watch('./src/css/sass/*.scss', gulp.series('sass'));
 });
 
-gulp.task("default", gulp.series(gulp.parallel("sass:watch")));
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  gulp.watch('./src/css/sass/*.scss', gulp.series('sass'));
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+
+
+gulp.task("default", gulp.series(gulp.parallel("browser-sync")));
