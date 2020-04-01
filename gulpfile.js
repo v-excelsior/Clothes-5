d614cc0ce1ce73e82c3e97ef8b45aec89ffe2c7a
vakyla98 <vakyla98@ukr.net>
2020-04-01 19:04:27 +0300

    Try pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,32 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var pug = require('gulp-pug');
 
-
-
-gulp.task('sass', async function () {
-  gulp.src('./src/css/sass/*.scss')
+gulp.task('sass', function () {
+  gulp.src('./src/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./build/css'))
     .pipe(browserSync.stream());;
 });
 
-gulp.task('sass:watch', async function () {
-  gulp.watch('./src/css/sass/*.scss', gulp.series('sass'));
+gulp.task('pug', function () {
+  return gulp.src("./src/pug/*.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest("./"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('browser-sync', function() {
+
+gulp.task('watchAll', function () {
   browserSync.init({
-      server: {
-          baseDir: "./"
-      }
+    server: {
+      baseDir: "./"
+    }
   });
-  gulp.watch('./src/css/sass/*.scss', gulp.series('sass'));
+  gulp.watch('./src/sass/*.scss', gulp.series('sass'));
+  gulp.watch('./src/pug/*.pug', gulp.series('pug'));
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
-
-
-gulp.task("default", gulp.series(gulp.parallel("browser-sync")));
+gulp.task("default", gulp.series(gulp.parallel("watchAll")));
--- src/js/lib.js
+++
@@ -1,3 +0,0 @@
-function mult(a,b){
-    return a * b;
-}
