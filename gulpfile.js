003fdc5e766ee0301cd2993fef56f208eedc7345
My Laptop <vakyla98@ukr.net>
2020-04-01 22:18:11 +0300

    New PUG trying

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,30 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var pug = require('gulp-pug');
 
-gulp.task('sass', function () {
-  gulp.src('./src/sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build/css'))
-    .pipe(browserSync.stream());;
+gulp.task('sass', function() {
+    return gulp.src('./src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('pug', function () {
-  return gulp.src("./src/pug/*.pug")
-    .pipe(pug({ pretty: true }))
-    .pipe(gulp.dest("./"))
-    .pipe(browserSync.stream());
+gulp.task('pug', function() {
+    return gulp.src("./src/pug/*.pug")
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest("./"))
+        .pipe(browserSync.stream());
 });
 
 
-gulp.task('watchAll', function () {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-  gulp.watch('./src/sass/*.scss', gulp.series('sass'));
-  gulp.watch('./src/pug/*.pug', gulp.series('pug'));
-  gulp.watch("./*.html").on('change', browserSync.reload);
+gulp.task('watchAll', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('./src/sass/*.scss', gulp.series('sass'));
+    gulp.watch('./src/pug/*.pug', gulp.series('pug'));
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 gulp.task("default", gulp.series(gulp.parallel("watchAll")));
