149b092a9eb608d1889675262ce22e3f98bf16cf
My Laptop <vakyla98@ukr.net>
2020-04-02 22:59:24 +0300

    add utility classes and media mixin

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('sass', function() {
 
 gulp.task('pug', function() {
     return gulp.src("./src/pug/*.pug")
-        .pipe(pug({ pretty: true }).on('error', pug.logError))
+        .pipe(pug({ pretty: true }))
         .pipe(gulp.dest("./dist"))
         .pipe(browserSync.stream());
 });
