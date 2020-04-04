8645dabd7545b18e9876b614bc19df050bf6928f
vakyla98 <vakyla98@ukr.net>
2020-04-04 16:52:33 +0300

    Create Header/Welcome sections.

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,58 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var pug = require('gulp-pug');
+var del = require("del");
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src('./src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('pug', function() {
+gulp.task('pug', function () {
     return gulp.src("./src/pug/*.pug")
         .pipe(pug({ pretty: true }))
         .pipe(gulp.dest("./dist"))
         .pipe(browserSync.stream());
 });
 
+gulp.task("clean", () => {
+    return del(["dist"]);
+});
 
-gulp.task('watchAll', function() {
+gulp.task("img", () => {
+    return gulp.src("src/img/**/*.*")
+        .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task("fonts", () => {
+    return gulp.src("src/fonts/**/*.*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task(
+    "build",
+    gulp.series(
+        "clean",
+        gulp.parallel(
+            "sass",
+            "pug",
+            "img",
+            "fonts"
+        )
+    )
+);
+
+gulp.task('watchAll', function () {
     browserSync.init({
         server: {
             baseDir: "./dist"
         }
     });
-    gulp.watch('./src/sass/*.scss', gulp.series('sass'));
-    gulp.watch('./src/pug/*.pug', gulp.series('pug'));
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
+    gulp.watch('./src/pug/**/*.pug', gulp.series('pug'));
+    gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
 gulp.task("default", gulp.series(gulp.parallel("watchAll")));
