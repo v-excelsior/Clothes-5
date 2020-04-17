72fe2d6505a4b1016974628c8272f6b14a7e3d77
vakyla98 <vakyla98@ukr.net>
2020-04-17 17:48:00 +0300

    123

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,96 +17,6 @@ const menu = document.querySelector('.header__nav-sections');
 let defaultMenuTop = menu.getBoundingClientRect().top;
 let curentMenuTop;
 
-// dragBtn.onmousedown = function (e) {
-//     e.preventDefault();
-//     let shiftY = e.clientY - e.target.getBoundingClientRect().top;
-//     console.log(shiftY);
-//     console.log(e.clientY);
-//     moveAt(e);
-
-//     function moveAt(e) {
-//         currentButtonPosition = e.clientY - shiftY;
-//         if (currentButtonPosition < headerHeight) {
-//             currentButtonPosition = headerHeight;
-//         }
-//         if (currentButtonPosition > 330) {
-//             currentButtonPosition = 330;
-//         }
-//         dragBtn.style.top = currentButtonPosition + 'px';
-//         sectionsList.style.top = currentButtonPosition - headerHeight - sectionsList.offsetHeight + 'px';
-//     }
-
-//     function onMouseMove(e) {
-//         moveAt(e);
-//     }
-
-//     document.addEventListener('mousemove', onMouseMove);
-
-//     document.onmouseup = function () {
-//         document.removeEventListener('mousemove', onMouseMove);
-//         document.onmouseup = null;
-//     };
-// };
-
-
-// dragBtn.addEventListener('touchstart', function (e) {
-//     e.preventDefault();
-//     let shiftY = e.touches[0].clientY - e.target.closest('div').getBoundingClientRect().top;
-
-//     moveAt(e);
-
-//     function moveAt(e) {
-//         currentButtonPosition = e.touches[0].clientY - shiftY;
-//         if (currentButtonPosition < 0) {
-//             currentButtonPosition = 0;
-//         }
-//         if (currentButtonPosition > 320) {
-//             currentButtonPosition = 320;
-//         }
-//         dragBtn.style.top = currentButtonPosition + 'px';
-//         sectionsList.style.top = currentButtonPosition - sectionsList.offsetHeight + 'px';
-//     }
-
-// // function drugToEnd(){
-// //     dragBtn.style.top = currentButtonPosition + 'px';
-// //     sectionsList.style.top = currentButtonPosition - sectionsList.offsetHeight + 'px';
-// // }
-// function dragToEnd(curentPos) {
-//     let posChanger = setInterval(() => {
-//         curentPos += 2;
-//         dragBtn.style.top = curentPos + 'px';
-//         sectionsList.style.top = curentPos - sectionsList.offsetHeight + 'px';
-//         if (curentPos >= 320) clearInterval(posChanger);
-//     },1)
-// }
-
-// function dragToStart(curentPos){
-//     let posChanger = setInterval(() => {
-//         curentPos -= 2;
-//         dragBtn.style.top = curentPos + 'px';
-//         sectionsList.style.top = curentPos - sectionsList.offsetHeight + 'px';
-//         if (curentPos <= 0) clearInterval(posChanger);
-//     },1)
-// }
-
-//     dragBtn.addEventListener('touchmove', moveAt);
-
-//     document.ontouchend = function () {
-//         console.log(currentButtonPosition)
-//         document.removeEventListener('touchmove', moveAt);
-//         document.touchend = null;
-//         if (currentButtonPosition > 150) {
-//             dragToEnd(currentButtonPosition);
-//         } else {
-//             dragToStart(currentButtonPosition);
-//         }
-//     };
-// });
-
-// dragBtn.ondragstart = function () {
-//     return false;
-// };
-
 dragBtn.addEventListener('touchstart', function (e) {
     e.preventDefault();
 
@@ -140,7 +50,8 @@ dragBtn.addEventListener('touchstart', function (e) {
         menu.ontransitionend = () => {
             menu.removeAttribute('style');
         }
-        setTimeout(() => { menu.removeAttribute('style');},300) // prevent single click without move
+        setTimeout(() => { menu.removeAttribute('style');},300)
+        // prevent single click without move
     };
 });
 
